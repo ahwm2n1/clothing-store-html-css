d24025fa0583dbd58fd5704bb10379eae75d1048
Ahmed Arfan <bfna14js@gmail.com>
2025-12-31 02:40:51 +0500

    Add files via upload

---
+++ Project2/script.js
@@ -0,0 +1,9 @@
+console.log("My Name is Ahmed Arfan ");
+alert("My  Name is Ahmed Arfan");
+const student = {
+    fullname: "AhmedArfan",
+    age: 21,
+    cgpa: 8.1,
+    ispass: true,
+};
+student["age"] = student["age"]+ 1
